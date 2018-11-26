c7db999236f365f440048a012c89a256fea6e285
root <root@glassmirror.net>
2018-11-26 04:24:43 -0500

    Fixed error where hidden nav links prevented usage of icon links.

--- www/header.js
+++ www/header.js
@@ -2,10 +2,10 @@ var open = false;
 
 function toggleMobileNavLinks(){
 		if(open){
-			document.getElementById("mobileNavLinks").style.visibility = "hidden";
+			document.getElementById("mobileNavLinks").style.display = "none";
 			open = false;
 		}else{
-			document.getElementById("mobileNavLinks").style.visibility = "visible";	
+			document.getElementById("mobileNavLinks").style.display = "block";
 			open = true;
 		}
 }
