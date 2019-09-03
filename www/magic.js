91bad5e3c4d3c2e440c2d154275fca3901ce9120
rnordman <28953646+nordmanr@users.noreply.github.com>
2019-09-03 12:37:23 -0700

    Magic page made consistent with formatting on items and weapons page

--- www/magic.js
+++ www/magic.js
@@ -1,17 +1,22 @@
 function onLoad(){
-	var url = window.location.href;
-	var id = url.split("#")[1];
-	var finder = document.getElementById(id);
-	var details = finder.nextElementSibling;
-	details.style.outline = "thin solid gold";
-}
-
-function onPress(){
-	var list = document.getElementsByTagName("tr");
-	for(var i=0; i<list.length; i++){
-		list[i].style.outline = "";
+	try{
+		var url = window.location.href;
+		var id = url.split("#")[1];
+		var finder = document.getElementById(id);
+		var details = finder.nextElementSibling;
+		details.style.outline = "thin solid gold";
+	}catch(err){}
 	}
-	onLoad();
-}
-
-window.setInterval(onPress, 50);
+	
+	function onPress(){
+		var list = document.getElementsByTagName("tr");
+		for(var i=0; i<list.length; i++){
+			list[i].style.outline = "";
+		}
+		try{
+		onLoad();
+		}catch(err){}
+	}
+	
+	window.setInterval(onPress, 50);
+	
