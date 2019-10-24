e9163d8403741b154cc36e899f080687b12ffa14
thomas <rnordman+github@protonmail.com>
2019-10-23 22:30:04 -0700

    Added bow-like weapons to database

--- sf2/shiningforce2_sql.sh
+++ sf2/shiningforce2_sql.sh
@@ -12,17 +12,34 @@ USE shiningforce2;
 CREATE TABLE `weapons` (
     `id` INT NOT NULL AUTO_INCREMENT, 
     `name` VARCHAR(50) NOT NULL, 
-    `class` SET('SDMN','HERO','PRST','VICR','MMNK','KNTE','PLDN','PGNT','WARR','GLDT','BRN','MAGE','WIZ','SORC','THIF','NINJ','TORT','MNST','PHNK','PHNX','RNGR','BWNT','WFMN','WFBR','BDMN','BDBT','BRGN','ACHR','SNIP','RBT','GLM','RDBN'),
-    `att` TINYINT,
-    `def` TINYINT,
-    `agi` TINYINT,
-    `mov` TINYINT,
-    `cost` SMALLINT,
+    `class` SET('SDMN','HERO','PRST','VICR','MMNK','KNTE','PLDN','PGNT','WARR','GLDT','BRN','MAGE','WIZ','SORC','THIF','NINJ','TORT','MNST','PHNK','PHNX','RNGR','BWNT','WFMN','WFBR','BDMN','BDBT','BRGN','ACHR','SNIP','RBT','GLM','RDBN') NOT NULL,
+    `att` TINYINT NOT NULL,
+    `def` TINYINT NOT NULL,
+    `agi` TINYINT NOT NULL,
+    `mov` TINYINT NOT NULL,
+    `cost` SMALLINT NOT NULL,
     `cursed` BIT(1) NOT NULL,
     `mithril` BIT(1) NOT NULL,
-    `description` VARCHAR(250),    
+    `description` VARCHAR(250) NOT NULL,    
     PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO `shiningforce2`.`characters` (`id`,`name`) VALUES ('1', 'utopple.com'), ('2', 'mail.utopple.com'), ('3', 'mail.local.utopple.com'), ('4', 'local.utopple.com');
+INSERT INTO `shiningforce2`.`weapons` (
+    `name`,`class`,`att`,`def`,`agi`,`mov`,`cost`,`cursed`,`mithril`,`description`)
+    VALUES
+    ('Wooden Arrow','ACHR,BRGN,SNIP,RNGR,BWNT',5,0,0,0,250,0,0,''),
+    ('Iron Arrow','ACHR,BRGN,SNIP,RNGR,BWNT',7,0,0,0,600,0,0,''),
+    ('Steel Arrow','ACHR,BRGN,SNIP,RNGR,BWNT',12,0,0,0,1270,0,0,''),
+    ('Robin Arrow','BRGN,SNIP,BWNT',17,0,0,0,1480,0,0,''),
+    ('Assault Shell','BRGN,SNIP,BWNT',25,0,0,0,2500,0,0,''),
+    ('Great Shot','BRGN,SNIP,BWNT',29,0,0,0,5000,0,0,''),
+    ('Nazca Cannon','BRGN,SNIP,BWNT',33,0,0,0,3000,0,0,'Found on the Nazca Ship'),
+    ('Buster Shot','BRGN,SNIP,BWNT',37,0,0,0,6800,0,1,''),
+    ('Hyper Cannon','BRGN,SNIP,BWNT',40,0,0,0,8700,0,1,''),
+    ('Grand Cannon','BRGN,SNIP,BWNT',43,0,0,0,9800,0,1,'Use to cast Muddle 1'),
+    ('Evil Shot','BRGN,SNIP,BWNT',51,-5,0,0,13000,1,0,'');
+
+
+
+
 EOF
 
 `weapons`
