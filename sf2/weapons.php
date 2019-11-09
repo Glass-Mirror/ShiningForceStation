63686f808211efcb88d5afca3ee436381f6610a1
thomas <rnordman+github@protonmail.com>
2019-11-08 22:17:44 -0700

    Display most of the information on screen. Also setup for easy shift to universal page later.

--- sf2/weapons.php
+++ sf2/weapons.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <?php
-        // which table are we using?
+<?php
+        $pageTitle = $_GET["title"];
 
         // has user and password info for MySQL SELECT.
         $configFile = fopen("config.txt", "r") or die("Unable to open file!");
@@ -26,7 +25,7 @@
             die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql = "SELECT * FROM weapons";
+        $sql = "SELECT * FROM ".strtolower($pageTitle);
         $result = $conn->query($sql);
         $conn->close();
 
@@ -38,56 +37,80 @@
             array_push($records, $row);
         }
     ?>
-
-    <title>Weapons</title>
+<head>
+    <title><?php echo ucfirst($pageTitle) ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="universal.css">
+    <script src="header.js"></script>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
+    <?php
+        include 'universal.php';
+        echo loadHeader();?>
     <div id="content">
-        <div id="overviewTable">
-            <?php
-            $amt = sizeof($records);
-            $widthOfTable = floor(sqrt($amt));
-            
-            // keep track of repeated classes
-            $classesChecked = array();
-            
-            foreach($records as $record){
-                $classsList = $record["class"];
-                $classTypes = explode(",",$classsList); // classes for this weapon
-                
-                $newClass = true;
+        <div id="overviewTableWrapper">
+            <div id="overviewTable" class="fancyBorder">
+                <?php
+                    $amt = sizeof($records);
+                    $widthOfTable = floor(sqrt($amt));
+                    
+                    $count=0; // used to break up table into square-ish
 
-                foreach($classTypes as $checkRepeat){
-                    if(in_array($checkRepeat, $classesChecked)){
-                        $newClass = false; // repeat class
-                    }else{
-                        array_push($classesChecked, $checkRepeat); // add to seen class list
+                    if(ceil($amt/$widthOfTable) > 7){ // maximum of 7 rows.
+                        $widthOfTable = ceil($amt/7);
                     }
-                }
+                    
+                    echo "<table><tr>";
 
-                if($newClass){
-                    echo "<br />";
-                }
-                echo "<img style='width:4vw' src='.".$record["filepath"].".png"."'></img>";
-            }
-            
+                    foreach($records as $record){
+                        // insert image and link to lower location on page
+                        echo "<td><a href=#".strtolower(str_replace(" ","",$record["name"]))."><img src='.".$record["filepath"].".png"."'></img></a></td>";
+                        $count ++;
 
-            foreach($records as $row){
-                echo "<br/>name: " . $row["name"]. " - class: " . $row["class"];
-            }
-            ?>
+                        // if width of table, add a newline
+                        if($count==$widthOfTable){
+                            echo "</tr><tr>";
+                            $count=0;
+                        }
+                    }
+                    echo "</tr></table>";
+                ?>
+            </div>
         </div>
-
         <div id="details">
+            <table class="fancyBorder">
+                <tr>
+                    <th>Icon</th>
+                    <th>Name</th>
+                    <th>Users</th>
+                    <th>Attack</th>
+                    <th>Defense</th>
+                    <th>Agility</th>
+                    <th>Movement</th>
+                    <th>Notes</th>
+                </tr>
+                <?php
+                    foreach($records as $row){
+                        echo "<tr><td><img src=\".".$row["filepath"].
+                            ".png\"/></td><td>".$row["name"].
+                            "</td><td>".$row["class"].
+                             "</td><td>".$row["att"].
+                             "</td><td>".$row["def"].
+                             "</td><td>".$row["agi"].
+                             "</td><td>".$row["mov"].
+                             "</td><td>".$row["description"].
+                             "</td></tr>";
+                    }
 
+                ?>
+            </table>
         </div>
-
     </div>
     <?php
-    
-
-
-    
-    ?>
+        include 'universal.php';
+        echo loadFooter();?>
 </body>
 </html>
